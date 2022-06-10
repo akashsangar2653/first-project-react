fa9177ff0a550f32404b78234da9851141875927
Akash Sangar <akashsangar2653@gmail.com>
2022-06-10 10:29:44 +0530

    created till image of web

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import { Article, Brand, CTA, Feature, Navbar} from './components';
 import { Features, Blog, Footer, Header, Possibility, Whatprojectonefolder} from './containers';
+import './App.css'
+import './index.css'
 const App = () => {
   return (
-    <div>practice_one_folder</div>
+    <div className='App'>
+      <div className='gradient_bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Whatprojectonefolder />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+
   )
 }
 

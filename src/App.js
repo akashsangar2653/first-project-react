cbc05bdcc7cbab29c8ecedde19e64f72e6303b75
Akash Sangar <akashsangar2653@gmail.com>
2022-06-10 08:14:06 +0530

    on 10th june after making containers and components with js andcss

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Article, Brand, CTA, Feature, Navbar} from './components';
+import { Features, Blog, Footer, Header, Possibility, Whatprojectonefolder} from './containers';
+const App = () => {
+  return (
+    <div>practice_one_folder</div>
+  )
+}
+
+export default App
---
+++ src/containers/whatprojectonefolder/Whatprojectonefolder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatprojectonefolder.css'
+const Whatprojectonefolder = () => {
+  return (
+    <div>Whatprojectonefolder</div>
+  )
+}
+
+export default Whatprojectonefolder
